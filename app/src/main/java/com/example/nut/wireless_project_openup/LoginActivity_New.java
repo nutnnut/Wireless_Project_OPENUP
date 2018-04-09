c2e1f85097b2711d8df46b40da3939863a582d4b
bampapi01 <bam.papi01@gmail.com>
2018-04-09 20:45:43 +0700

    New login activity

---
+++ app/src/main/java/com/example/nut/wireless_project_openup/LoginActivity_New.java
@@ -0,0 +1,99 @@
+package com.example.nut.wireless_project_openup;
+
+import android.annotation.TargetApi;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TextInputEditText;
+import android.support.design.widget.TextInputLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatButton;
+import android.support.v7.widget.AppCompatTextView;
+import android.view.View;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.TextView;
+
+import helpers.InputValidation;
+import sql.DatabaseHelper;
+
+import static android.Manifest.permission.READ_CONTACTS;
+
+/**
+ * Created by BAMBOOK on 4/9/2018.
+ */
+
+public class LoginActivity_New extends AppCompatActivity implements View.OnClickListener{
+
+    private static final int REQUEST_READ_CONTACTS = 0;
+    private final AppCompatActivity activity = LoginActivity_New.this;
+
+    private TextInputLayout textInputLayoutEmail;
+    private TextInputLayout textInputLayoutPassword;
+
+    private AutoCompleteTextView autoCompleteTextViewEmail;
+    private TextInputEditText textInputEditTextPassword;
+
+    private Button appCompatButtonLogin;
+
+    private TextView textViewLinkRegister;
+
+    private InputValidation inputValidation;
+    private DatabaseHelper databaseHelper;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        getSupportActionBar().hide();
+
+        autoCompleteTextViewEmail = (AutoCompleteTextView) findViewById(R.id.email);
+        populateAutoComplete();
+
+
+        //initViews();
+        //initListeners();
+        //initObjects();
+    }
+
+    private void populateAutoComplete() {
+        if (!mayRequestContacts()) {
+            return;
+        }
+
+        getLoaderManager().initLoader(0, null, this);
+    }
+
+    private boolean mayRequestContacts() {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            return true;
+        }
+        if (checkSelfPermission(READ_CONTACTS) == PackageManager.PERMISSION_GRANTED) {
+            return true;
+        }
+        if (shouldShowRequestPermissionRationale(READ_CONTACTS)) {
+            Snackbar.make(autoCompleteTextViewEmail, R.string.permission_rationale, Snackbar.LENGTH_INDEFINITE)
+                    .setAction(android.R.string.ok, new View.OnClickListener() {
+                        @Override
+                        @TargetApi(Build.VERSION_CODES.M)
+                        public void onClick(View v) {
+                            requestPermissions(new String[]{READ_CONTACTS}, REQUEST_READ_CONTACTS);
+                        }
+                    });
+        } else {
+            requestPermissions(new String[]{READ_CONTACTS}, REQUEST_READ_CONTACTS);
+        }
+        return false;
+    }
+
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+}
