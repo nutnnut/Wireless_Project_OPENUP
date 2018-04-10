b12703ad4ff0bb3d950dac5bfb3dca93f46644dc
bampapi01 <bam.papi01@gmail.com>
2018-04-10 15:52:11 +0700

    Add Register Activity

---
+++ app/src/main/java/com/example/nut/wireless_project_openup/UserListActivity.java
@@ -0,0 +1,13 @@
+package com.example.nut.wireless_project_openup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UserListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_list);
+    }
+}
