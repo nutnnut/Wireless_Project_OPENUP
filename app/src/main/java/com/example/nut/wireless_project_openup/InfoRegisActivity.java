d8a4ace4457556364d361f32dcba24c7f8f7c573
bampapi01 <bam.papi01@gmail.com>
2018-04-14 00:54:36 +0700

    add SessionManager class

--- app/src/main/java/com/example/nut/wireless_project_openup/InfoRegisActivity.java
+++ app/src/main/java/com/example/nut/wireless_project_openup/InfoRegisActivity.java
@@ -106,6 +106,6 @@ public class InfoRegisActivity extends AppCompatActivity{
     }
 
     public void postDataToSQLite(){
-        
+
     }
 }
---
+++ app/src/main/java/helpers/SessionManager.java
@@ -0,0 +1,120 @@
+package helpers;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+
+import com.example.nut.wireless_project_openup.LoginActivity_New;
+
+import java.util.HashMap;
+
+/**
+ * Created by BAMBOOK on 4/13/2018.
+ */
+
+public class SessionManager {
+    // Shared Preferences
+    SharedPreferences pref;
+
+    // Editor for Shared preferences
+    SharedPreferences.Editor editor;
+
+    // Context
+    Context _context;
+
+    // Shared pref mode
+    int PRIVATE_MODE = 0;
+
+    // Sharedpref file name
+    private static final String PREF_NAME = "OpenupPref";
+
+    // All Shared Preferences Keys
+    private static final String IS_LOGIN = "IsLoggedIn";
+
+    // User name (make variable public to access from outside)
+    public static final String KEY_ID = "userID";
+
+
+    // Constructor
+    public SessionManager(Context context){
+        this._context = context;
+        pref = _context.getSharedPreferences(PREF_NAME, PRIVATE_MODE);
+        editor = pref.edit();
+    }
+
+    /**
+     * Create login session
+     * */
+    public void createLoginSession(Integer ID){
+        // Storing login value as TRUE
+        editor.putBoolean(IS_LOGIN, true);
+
+        // Storing name in pref
+        editor.putInt(KEY_ID, ID);
+
+        // commit changes
+        editor.commit();
+    }
+
+    /**
+     * Check login method wil check user login status
+     * If false it will redirect user to login page
+     * Else won't do anything
+     * */
+    public void checkLogin(){
+        // Check login status
+        if(!this.isLoggedIn()){
+            // user is not logged in redirect him to Login Activity
+            Intent i = new Intent(_context, LoginActivity_New.class);
+            // Closing all the Activities
+            i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+            // Add new Flag to start new Activity
+            i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+            // Staring Login Activity
+            _context.startActivity(i);
+        }
+
+    }
+
+
+
+    /**
+     * Get stored session data
+     * */
+    public Integer getUserID(){
+        Integer userID = pref.getInt(KEY_ID, 0);
+        // return userID
+        return userID;
+    }
+
+    /**
+     * Clear session details
+     * */
+    public void logoutUser(){
+        // Clearing all data from Shared Preferences
+        editor.clear();
+        editor.commit();
+
+        // After logout redirect user to Loing Activity
+        Intent i = new Intent(_context, LoginActivity_New.class);
+        // Closing all the Activities
+        i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+        // Add new Flag to start new Activity
+        i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+        // Staring Login Activity
+        _context.startActivity(i);
+    }
+
+    /**
+     * Quick check for login
+     * **/
+    // Get Login State
+    public boolean isLoggedIn(){
+        return pref.getBoolean(IS_LOGIN, false);
+    }
+}
+
