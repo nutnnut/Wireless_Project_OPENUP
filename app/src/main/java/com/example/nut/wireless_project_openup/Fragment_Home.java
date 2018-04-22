8b51c0b5908296540ecf3724b858c3da954dfa09
nutnnut <nut.euangkanakul@gmail.com>
2018-04-22 21:20:43 +0700

    VideoCall camera

--- app/src/main/java/com/example/nut/wireless_project_openup/Fragment_Home.java
+++ app/src/main/java/com/example/nut/wireless_project_openup/Fragment_Home.java
@@ -1,8 +1,11 @@
 package com.example.nut.wireless_project_openup;
 
 import android.app.Fragment;
+import android.app.FragmentManager;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,11 +13,21 @@ import android.view.ViewGroup;
 public class Fragment_Home extends Fragment {
 
     View myView;
+    CardView videoshortcut;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         myView = inflater.inflate(R.layout.layout_home, container, false);
+
+        videoshortcut = (CardView) myView.findViewById(R.id.videoshortcut);
+        videoshortcut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                Intent myIntent = new Intent(getActivity(),VideoCall.class);
+                startActivity(myIntent);
+            }
+        });
         return myView;
     }
 }
--- app/src/main/java/com/example/nut/wireless_project_openup/VideoCall.java
+++ app/src/main/java/com/example/nut/wireless_project_openup/VideoCall.java
@@ -1,13 +1,70 @@
 package com.example.nut.wireless_project_openup;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.hardware.Camera;
 
 public class VideoCall extends AppCompatActivity {
+
+    private Camera mCamera;
+    private CameraPreview mPreview;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_chatroom);
+        setContentView(R.layout.activity_videocall);
+
+        //RequestPermission if needed
+        if (ContextCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.CAMERA}, 50);
+        }
+
+        // Create an instance of Camera
+        mCamera = getCameraInstance();
+        if(mCamera!=null){
+            // Create our Preview view and set it as the content of our activity.
+            mPreview = new CameraPreview(this, mCamera);
+            FrameLayout preview = (FrameLayout) findViewById(R.id.videofeed);
+            preview.addView(mPreview);
+        }
+        else{
+            Log.d("Camera", "NULL!!!!");
+        }
+
+    }
+
+    /** A safe way to get an instance of the Camera object. */
+    public static Camera getCameraInstance(){
+        Camera c = null;
+        try {
+            c = Camera.open(); // attempt to get a Camera instance
+        }
+        catch (Exception e){
+            // Camera is not available (in use or does not exist)
+        }
+        return c; // returns null if camera is unavailable
+    }
+
+    public void EndCall(View view){
+        if(mCamera!=null)mCamera.release();
+        super.onBackPressed();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(mCamera!=null)mCamera.release();
+        super.onBackPressed();
     }
 }
