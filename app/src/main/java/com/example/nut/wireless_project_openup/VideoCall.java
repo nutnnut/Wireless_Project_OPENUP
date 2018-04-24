fcb52ef78f6972bdab725936b20079282f187856
midasang <manasanan.sir@gmail.com>
2018-04-24 16:18:31 +0700

    กล้องหมุนตามจอ

--- app/src/main/java/com/example/nut/wireless_project_openup/VideoCall.java
+++ app/src/main/java/com/example/nut/wireless_project_openup/VideoCall.java
@@ -1,24 +1,26 @@
 package com.example.nut.wireless_project_openup;
 
+import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
+import android.hardware.Camera;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraManager;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Surface;
 import android.view.View;
 import android.widget.FrameLayout;
-import android.widget.ImageView;
-import android.hardware.Camera;
 
 public class VideoCall extends AppCompatActivity {
 
     private Camera mCamera;
     private CameraPreview mPreview;
+    private int cameraID;
 
 
     @Override
@@ -45,7 +47,24 @@ public class VideoCall extends AppCompatActivity {
         else{
             Log.d("Camera", "NULL!!!!");
         }
+        setupCamera(1536,2048);
+        setCameraDisplayOrientation(this,cameraID,mCamera);
 
+    }
+
+    private  void setupCamera(int width,int height)
+    {
+        CameraManager Manager=(CameraManager) getSystemService(Context.CAMERA_SERVICE);
+        try{
+            for(String c:Manager.getCameraIdList()){
+                CameraCharacteristics characteristics=Manager.getCameraCharacteristics(c);
+                if(characteristics.get(characteristics.LENS_FACING)==characteristics.LENS_FACING_FRONT){
+                    continue;
+                }cameraID=Integer.parseInt(c);
+            }
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
     }
 
     /** A safe way to get an instance of the Camera object. */
@@ -69,5 +88,30 @@ public class VideoCall extends AppCompatActivity {
     public void onBackPressed() {
         if(mCamera!=null)mCamera.release();
         super.onBackPressed();
+    }
+
+    public static void setCameraDisplayOrientation(Activity activity,
+                                                   int cameraId, android.hardware.Camera camera) {
+        android.hardware.Camera.CameraInfo info =
+                new android.hardware.Camera.CameraInfo();
+        android.hardware.Camera.getCameraInfo(cameraId, info);
+        int rotation = activity.getWindowManager().getDefaultDisplay()
+                .getRotation();
+        int degrees = 0;
+        switch (rotation) {
+            case Surface.ROTATION_0: degrees = 0; break;
+            case Surface.ROTATION_90: degrees = 90; break;
+            case Surface.ROTATION_180: degrees = 180; break;
+            case Surface.ROTATION_270: degrees = 270; break;
+        }
+
+        int result;
+        if (info.facing == Camera.CameraInfo.CAMERA_FACING_FRONT) {
+            result = (info.orientation + degrees) % 360;
+            result = (360 - result) % 360;  // compensate the mirror
+        } else {  // back-facing
+            result = (info.orientation - degrees + 360) % 360;
+        }
+        camera.setDisplayOrientation(result);
     }
 }
