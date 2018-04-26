daf6e3702fd9dea70c3b13784be73e0cbe3a8c74
nutnnut <nut.euangkanakul@gmail.com>
2018-04-26 22:19:02 +0700

    Switch Camera

--- app/src/main/java/com/example/nut/wireless_project_openup/VideoCall.java
+++ app/src/main/java/com/example/nut/wireless_project_openup/VideoCall.java
@@ -2,6 +2,7 @@ package com.example.nut.wireless_project_openup;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.hardware.Camera;
 import android.hardware.camera2.CameraAccessException;
@@ -15,6 +16,7 @@ import android.util.Log;
 import android.view.Surface;
 import android.view.View;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class VideoCall extends AppCompatActivity {
@@ -22,12 +24,23 @@ public class VideoCall extends AppCompatActivity {
     private Camera mCamera;
     private CameraPreview mPreview;
     private int cameraID;
-
+    private String TAG = "VideoCall";
+    FrameLayout previewlayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_videocall);
+
+        //Switch Camera Button
+        ImageView SwitchCamera = (ImageView) findViewById(R.id.SwitchCamera);
+        // set a onclick listener for when the button gets clicked
+        SwitchCamera.setOnClickListener(new View.OnClickListener() {
+            // Start new list activity
+            public void onClick(View v) {
+                SwitchCamera();
+            }
+        });
 
         //RequestPermission if needed
         if (ContextCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
@@ -38,27 +51,16 @@ public class VideoCall extends AppCompatActivity {
         }
 
         // Create an instance of Camera
-//        mCamera = getCameraInstance();
-//        if(mCamera!=null){
-//            // Create our Preview view and set it as the content of our activity.
-//            mPreview = new CameraPreview(this, mCamera);
-//            FrameLayout preview = (FrameLayout) findViewById(R.id.videofeed);
-//            preview.addView(mPreview);
-//        }
-//        else{
-//            Log.d("Camera", "NULL!!!!");
-//        }
-
-        int cameraId = findFrontFacingCamera();
-        if (cameraId < 0) {
+        cameraID = findFrontFacingCamera();
+        if (cameraID < 0) {
             Toast.makeText(this, "No front facing camera found.",
                     Toast.LENGTH_LONG).show();
         } else {
-            mCamera = Camera.open(cameraId);
+            mCamera = Camera.open(cameraID);
             //Create our Preview view and set it as the content of our activity.
             mPreview = new CameraPreview(this, mCamera);
-            FrameLayout preview = (FrameLayout) findViewById(R.id.videofeed);
-            preview.addView(mPreview);
+            previewlayout = (FrameLayout) findViewById(R.id.videofeed);
+            previewlayout.addView(mPreview);
         }
 
         setupCamera(1536,2048);
@@ -67,7 +69,46 @@ public class VideoCall extends AppCompatActivity {
     }
 
     private void SwitchCamera(){
-        
+        //Find info
+        Camera.CameraInfo info = new Camera.CameraInfo();
+        Camera.getCameraInfo(cameraID, info);
+
+        //Clear current camera
+        if(mCamera!=null)mCamera.release();
+        previewlayout.removeAllViews();
+
+        //Currently front, switch to back
+        if (info.facing == Camera.CameraInfo.CAMERA_FACING_FRONT) {
+
+            cameraID = findBackFacingCamera();
+            if (cameraID < 0) {
+                Toast.makeText(this, "No back facing camera found.",
+                        Toast.LENGTH_LONG).show();
+            } else {
+                mCamera = Camera.open(cameraID);
+                //Create our Preview view and set it as the content of our activity.
+                mPreview = new CameraPreview(this, mCamera);
+                FrameLayout preview = (FrameLayout) findViewById(R.id.videofeed);
+                preview.addView(mPreview);
+            }
+        }
+        //Currently back, switch to front
+        else{
+            cameraID = findFrontFacingCamera();
+            if (cameraID < 0) {
+                Toast.makeText(this, "No front facing camera found.",
+                        Toast.LENGTH_LONG).show();
+            } else {
+                mCamera = Camera.open(cameraID);
+                //Create our Preview view and set it as the content of our activity.
+                mPreview = new CameraPreview(this, mCamera);
+                FrameLayout preview = (FrameLayout) findViewById(R.id.videofeed);
+                preview.addView(mPreview);
+            }
+        }
+
+        setupCamera(1536,2048);
+        setCameraDisplayOrientation(this,cameraID,mCamera);
     }
 
     private int findFrontFacingCamera() {
@@ -85,19 +126,35 @@ public class VideoCall extends AppCompatActivity {
         return cameraId;
     }
 
+    private int findBackFacingCamera() {
+        int cameraId = -1;
+        // Search for the front facing camera
+        int numberOfCameras = Camera.getNumberOfCameras();
+        for (int i = 0; i < numberOfCameras; i++) {
+            Camera.CameraInfo info = new Camera.CameraInfo();
+            Camera.getCameraInfo(i, info);
+            if (info.facing == Camera.CameraInfo.CAMERA_FACING_BACK) {
+                cameraId = i;
+                break;
+            }
+        }
+        return cameraId;
+    }
+
     private  void setupCamera(int width,int height)
     {
-        CameraManager Manager=(CameraManager) getSystemService(Context.CAMERA_SERVICE);
-        try{
-            for(String c:Manager.getCameraIdList()){
-                CameraCharacteristics characteristics=Manager.getCameraCharacteristics(c);
-                if(characteristics.get(characteristics.LENS_FACING)==characteristics.LENS_FACING_FRONT){
-                    continue;
-                }cameraID=Integer.parseInt(c);
-            }
-        } catch (CameraAccessException e) {
-            e.printStackTrace();
-        }
+        //What is this even for???
+//        CameraManager Manager=(CameraManager) getSystemService(Context.CAMERA_SERVICE);
+//        try{
+//            for(String c:Manager.getCameraIdList()){
+//                CameraCharacteristics characteristics=Manager.getCameraCharacteristics(c);
+//                if(characteristics.get(characteristics.LENS_FACING)==characteristics.LENS_FACING_FRONT){
+//                    continue;
+//                }cameraID=Integer.parseInt(c);
+//            }
+//        } catch (CameraAccessException e) {
+//            e.printStackTrace();
+//        }
     }
 
     /** A safe way to get an instance of the Camera object. */
@@ -123,8 +180,7 @@ public class VideoCall extends AppCompatActivity {
         super.onBackPressed();
     }
 
-    public static void setCameraDisplayOrientation(Activity activity,
-                                                   int cameraId, android.hardware.Camera camera) {
+    public static void setCameraDisplayOrientation(Activity activity, int cameraId, android.hardware.Camera camera) {
         android.hardware.Camera.CameraInfo info =
                 new android.hardware.Camera.CameraInfo();
         android.hardware.Camera.getCameraInfo(cameraId, info);
