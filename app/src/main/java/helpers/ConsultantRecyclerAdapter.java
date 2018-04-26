a4c366afe5fc123fb87a92296ab264be797f4390
bampapi01 <bam.papi01@gmail.com>
2018-04-26 21:31:28 +0700

    ..

--- app/src/main/java/helpers/ConsultantRecyclerAdapter.java
+++ app/src/main/java/helpers/ConsultantRecyclerAdapter.java
@@ -3,6 +3,8 @@ package helpers;
 /**
  * Created by BAMBOOK on 4/18/2018.
  */
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.widget.AppCompatTextView;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
@@ -10,6 +12,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.nut.wireless_project_openup.Chatroom;
+import com.example.nut.wireless_project_openup.ConsultantListActivity;
 import com.example.nut.wireless_project_openup.R;
 
 import java.util.List;
@@ -39,6 +43,7 @@ public class ConsultantRecyclerAdapter extends RecyclerView.Adapter<ConsultantRe
 
     @Override
     public void onBindViewHolder(ConsultantViewHolder holder, int position) {
+        holder.consultantID = list.get(position).getConsultantID();
         holder.textViewName.setText(list.get(position).getName());
         holder.textViewGender.setText(list.get(position).getGender());
         holder.textViewAge.setText(list.get(position).getAge().toString());
@@ -55,19 +60,28 @@ public class ConsultantRecyclerAdapter extends RecyclerView.Adapter<ConsultantRe
     /**
      * ViewHolder class
      */
-    public class ConsultantViewHolder extends RecyclerView.ViewHolder {
+    public class ConsultantViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
 
         public AppCompatTextView textViewName;
         public AppCompatTextView textViewGender;
         public AppCompatTextView textViewAge;
         public AppCompatTextView textViewExpertise;
+        private Integer consultantID;
 
         public ConsultantViewHolder(View view) {
             super(view);
+            view.setOnClickListener(this);
             textViewName = (AppCompatTextView) view.findViewById(R.id.textViewName);
             textViewGender = (AppCompatTextView) view.findViewById(R.id.textViewGender);
             textViewAge = (AppCompatTextView) view.findViewById(R.id.textViewAge);
             textViewExpertise = (AppCompatTextView) view.findViewById(R.id.textViewExpertise);
+        }
+
+        @Override
+        public void onClick(View view) {
+            //final Intent listIntent = new Intent(, Chatroom.class);
+
+            final Bundle bundle = new Bundle();
         }
     }
 }
